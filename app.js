5a5b30ea7f8ee1b608a07254e33e03a26da9bb3c
Inderjit Giri <inderjitgiri2002@gmail.com>
2026-05-13 22:34:16 +0530

    install the NanoId package for generate the random shortId

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
+const { createShortUrl } = require("./routes/url.router");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", createShortUrl);
 
 module.exports = app;
---
+++ controllers/url.controller.js
@@ -0,0 +1,7 @@
+async function createShortUrl(req, res) {
+  try {
+    const { url } = req.body;
+
+    const shortId
+  } catch (err) {}
+}
